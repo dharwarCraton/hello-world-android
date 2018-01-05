bac2dd312c3dbfd5bf09a332bed32a1858ada3f9
dharwarCraton <fnalam@bsugmail.net>
2018-01-05 16:16:36 -0500

    Added comment to main activity

---
+++ android-hello-world/app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sqisland.android.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Third Try
+ */
+public class MainActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
