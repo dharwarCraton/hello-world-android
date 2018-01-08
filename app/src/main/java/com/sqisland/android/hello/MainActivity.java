1b6148699d88be90afb65ab741034a09ba85b2ca
dharwarCraton <fnalam@bsugmail.net>
2018-01-08 16:08:54 -0500

    5: instrumentation testing

--- app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -3,12 +3,17 @@ package com.sqisland.android.hello;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+  private TextView greetingView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    greetingView = (TextView) findViewById(R.id.greeting);
   }
 
   @Override
@@ -16,4 +21,15 @@ public class MainActivity extends Activity {
     getMenuInflater().inflate(R.menu.activity_main, menu);
     return true;
   }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == R.id.action_bye) {
+      greetingView.setText(R.string.bye);
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+
 }
